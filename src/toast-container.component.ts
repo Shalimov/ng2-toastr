ee99a76f50c25339e54e39512fa6248154f0144c
Igor <Shalimich@gmail.com>
2017-12-08 16:36:10 +0300

    Update toast-container.component.ts

--- src/toast-container.component.ts
+++ src/toast-container.component.ts
@@ -1,11 +1,12 @@
 import {
   Component, ChangeDetectorRef, transition, state, trigger, style, animate,
   NgZone, OnDestroy, AnimationTransitionEvent} from '@angular/core';
-import {Toast} from './toast';
-import {ToastOptions} from './toast-options';
+import {DomSanitizer} from '@angular/platform-browser';
 import 'rxjs/add/operator/first';
 import {Subject} from 'rxjs/Subject';
 import {Observable} from 'rxjs/Observable';
+import {Toast} from './toast';
+import {ToastOptions} from './toast-options';
 
 @Component({
   selector: 'toast-container',
@@ -110,7 +111,8 @@ export class ToastContainer implements OnDestroy {
   private _onEnter: Subject<any> = new Subject();
   private _onExit: Subject<any> = new Subject();
 
-  constructor(private cdr: ChangeDetectorRef,
+  constructor(public sanitizer: DomSanitizer,
+              private cdr: ChangeDetectorRef,
               private _zone: NgZone,
               options: ToastOptions)
   {
